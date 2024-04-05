e1e10a351be0dbd47dd5e34b8fa75356fb9f6961
Anubrata Sarkar <anubrata.sarkar@iemlabs.com>
2024-04-05 12:26:30 +0530

    js error solve

--- controller/Compiler.js
+++ controller/Compiler.js
@@ -140,7 +140,7 @@ exports.PythonRunner = async (code, input) => {
           "python3 " + filePath + " < " + inputPath,
           (err, stdout, stderr) => {
             const elapsedTime = Date.now() - startTime;
-           
+
             if (err) {
               console.error(`exec error: ${err}`);
               resolve({
@@ -149,11 +149,11 @@ exports.PythonRunner = async (code, input) => {
                 error: stderr,
               });
             }
-            
+
             resolve({
               err: false,
               output: stdout,
-              time: elapsedTime
+              time: elapsedTime,
             });
           }
         );
@@ -188,20 +188,23 @@ exports.JavaScriptRunner = async (code, input) => {
         const filePath = path.join(__dirname, "../d.js");
         console.log("javascript file -> " + filePath);
         const inputPath = path.join(__dirname, "../inputd.txt");
-        exec("node " + filePath + " < " + inputPath, (err, stdout, stderr) => {
-          if (err) {
-            console.error(`exec error: ${err}`);
+        exec(
+          "node " + filePath + " < " + inputPath,
+          (err, stdout, stderr) => {
+            if (err) {
+              console.error(`exec error: ${err}`);
+              resolve({
+                err: true,
+                output: err,
+                error: stderr,
+              });
+            }
             resolve({
-              err: true,
-              output: err,
-              error: stderr,
+              err: false,
+              output: stdout,
             });
           }
-          resolve({
-            err: false,
-            output: stdout,
-          });
-        });
+        );
       })
       .catch(() => {
         console.log("error saving javascript file \n" + saveFileRes);
